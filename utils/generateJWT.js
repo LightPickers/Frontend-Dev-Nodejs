dcb7802d853efb03ca9515c9ba13dc589c13cdb5
Tau Hsu <jason850629@gmail.com>
2025-04-24 19:53:53 +0800

    新增APIs * 2 (驗證登入、取得用戶資料)
    新增APIs * 2 (驗證登入、取得用戶資料)

--- utils/generateJWT.js
+++ utils/generateJWT.js
@@ -1,6 +1,8 @@
 //utils/generateJWT.js
 
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
+const AppError = require("./appError");
+const ERROR_MESSAGES = require("./errorMessages");
 
 /**
  * create JSON Web Token
@@ -9,20 +11,21 @@ const jwt = require('jsonwebtoken');
  * @param {Object} [option] same to npm package - jsonwebtoken
  * @returns {String}
  */
-module.exports = (payload, secret, option = {}) => new Promise((resolve, reject) => {
+module.exports = (payload, secret, option = {}) =>
+  new Promise((resolve, reject) => {
     jwt.sign(payload, secret, option, (err, token) => {
-        if (err) {
-            // reject(err)
-            // return
-            switch (err.name) {
-                case 'TokenExpiredError':
-                    reject(appError(401, 'Token 已過期'))
-                    break
-                default:
-                    reject(appError(401, '無效的 token'))
-                    break
-            }
+      if (err) {
+        // reject(err)
+        // return
+        switch (err.name) {
+          case "TokenExpiredError":
+            reject(new AppError(401, ERROR_MESSAGES.EXPIRED_TOKEN));
+            break;
+          default:
+            reject(new AppError(401, ERROR_MESSAGES.INVALID_TOKEN));
+            break;
         }
-        resolve(token)
-    })
-})
+      }
+      resolve(token);
+    });
+  });
