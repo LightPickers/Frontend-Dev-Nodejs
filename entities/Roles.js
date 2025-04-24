f9ccb87e7eb3a2664b6847f349e367096855d638
Hsiang <fdsa201305@gmail.com>
2025-04-24 14:42:09 +0800

    fix: 修改 Roles 資料表縮排

--- entities/Roles.js
+++ entities/Roles.js
@@ -1,35 +1,35 @@
-const { EntitySchema } = require('typeorm')
+const { EntitySchema } = require("typeorm");
 
 module.exports = new EntitySchema({
-    name: 'Roles',
-    tableName: 'ROLES',
-    columns: {
-        id: {
-            primary: true,
-            type: 'uuid',
-            generated: 'uuid'
-        },
-        name: {
-            type: 'varchar',
-            length: 50,
-            nullable: false
-        },
-        created_at: {
-            type: 'timestamp',
-            createDate: true,
-            nullable: false
-        },
-        updated_at: {
-            type: 'timestamp',
-            updateDate: true,
-            nullable: false
-        }
+  name: "Roles",
+  tableName: "ROLES",
+  columns: {
+    id: {
+      primary: true,
+      type: "uuid",
+      generated: "uuid",
     },
-    relations: {
-        Users: {
-          target: 'Users',
-          type: 'one-to-many',
-          inverseSide: 'Roles',
-        }
-      }
+    name: {
+      type: "varchar",
+      length: 50,
+      nullable: false,
+    },
+    created_at: {
+      type: "timestamp",
+      createDate: true,
+      nullable: false,
+    },
+    updated_at: {
+      type: "timestamp",
+      updateDate: true,
+      nullable: false,
+    },
+  },
+  relations: {
+    Users: {
+      target: "Users",
+      type: "one-to-many",
+      inverseSide: "Roles",
+    },
+  },
 });
