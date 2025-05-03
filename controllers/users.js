9d13548799ccc3ff3e34abc20c4972f80e39be39
Tau Hsu <jason850629@gmail.com>
2025-05-03 22:37:10 +0800

    users appError 大小寫更正
    users appError 大小寫更正

--- controllers/users.js
+++ controllers/users.js
@@ -4,7 +4,7 @@ const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const generateJWT = require("../utils/generateJWT");
 const logger = require("../utils/logger")("UsersController");
-const AppError = require("../utils/AppError");
+const AppError = require("../utils/appError");
 const ERROR_MESSAGES = require("../utils/errorMessages");
 
 const validateSignup = require("../utils/validateSignup");
