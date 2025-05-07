f99b25a84f05fb043bd991886a691ee18536ba37
Tau Hsu <jason850629@gmail.com>
2025-05-07 15:28:49 +0800

    ./utils/redis.js 修正、新增./config/redisSecret.js
    ./utils/redis.js 修正、新增./config/redisSecret.js

--- utils/redis.js
+++ utils/redis.js
@@ -1,8 +1,18 @@
 const { createClient } = require("redis");
-const redis = createClient(); // 預設會連到 localhost:6379
+const config = require("../config/index");
+const logger = require("./logger")("Redis");
 
-redis.on("error", (err) => console.error("Redis Client Error", err));
+const redis = createClient({
+  url: config.get("redisSecret.redisUrl"),
+});
 
-redis.connect(); // 回傳 Promise，你也可以在 app 啟動時 await redis.connect()
+redis.on("error", (err) => logger.error("Redis Client Error", err));
+
+async function connectRedis() {
+  await redis.connect();
+  logger.info("Redis connected!");
+}
+
+connectRedis();
 
 module.exports = redis;
