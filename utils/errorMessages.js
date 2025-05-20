04650b48959633f3198325958ca7f5db4e5ba830
Tau Hsu <jason850629@gmail.com>
2025-05-20 15:29:05 +0800

    utils/errorMessages.js 新增錯誤訊息：缺少 TradeInfo 或 TradeSha
    utils/errorMessages.js 新增錯誤訊息：缺少 TradeInfo 或 TradeSha

--- utils/errorMessages.js
+++ utils/errorMessages.js
@@ -40,6 +40,7 @@ const ERROR_MESSAGES = {
     "您已有尚未付款的相同商品訂單，請先完成付款",
   PAY_FAILED_TREADSHA_NOT_SAME: "付款失敗，TreadSha 不一致",
   COUPON_PERIOD_ERROR: "現在不在此優惠券使用期間，無法使用",
+  TRADEINFO_OR_TRADESHA_NOT_FOUND: "缺少 TradeInfo 或 TradeSha",
 };
 
 module.exports = ERROR_MESSAGES;
