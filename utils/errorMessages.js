658175161606325a91c45ff3e951c3efa83a1ac7
Tau Hsu <jason850629@gmail.com>
2025-05-24 16:23:11 +0800

    utils/errorMessages.js 新增錯誤訊息："您未被授權存取此訂單"
    utils/errorMessages.js 新增錯誤訊息："您未被授權存取此訂單"

--- utils/errorMessages.js
+++ utils/errorMessages.js
@@ -38,6 +38,7 @@ const ERROR_MESSAGES = {
   FINISH_CHECKOUT_FIRST: "請先完成結帳確認流程",
   ORDER_ALREADY_USED_PLEASE_PAY_FIRST:
     "您已有尚未付款的相同商品訂單，請先完成付款",
+  NOT_AUTHORIZED_FOR_ORDER: "您未被授權存取此訂單",
 };
 
 module.exports = ERROR_MESSAGES;
