7f089698ea84394e757182b233a06feda2ffa434
Angela-Chu <angelachu1598753@gmail.com>
2025-06-14 15:35:51 +0800

    調整controllers/精選分類，增加快取功能

--- controllers/category.js
+++ controllers/category.js
@@ -1,13 +1,15 @@
 const { dataSource } = require("../db/data-source");
 const logger = require("../utils/logger")("UsersController");
 const AppError = require("../utils/appError");
-const ERROR_MESSAGES = require("../utils/errorMessages");
+// const ERROR_MESSAGES = require("../utils/errorMessages");
 // const {
 //   isUndefined,
 //   isValidString,
 //   checkIfProductSaved,
 // } = require("../utils/validUtils");
+const { cacheOrFetch } = require("../utils/cache"); // 加入快取工具
 
+/*
 async function getFeaturedCategory(req, res, next) {
   const FeaturedCategoriesInfo = await dataSource
     .getRepository("Categories")
@@ -25,6 +27,35 @@ async function getFeaturedCategory(req, res, next) {
     status: true,
     data: FeaturedCategoriesInfo,
   });
+}
+*/
+
+async function getFeaturedCategory(req, res, next) {
+  try {
+    const FeaturedCategoriesInfo = await cacheOrFetch(
+      "homepage:featuredCategories", // Redis key
+      async () => {
+        return await dataSource.getRepository("Categories").find({
+          select: {
+            id: true,
+            name: true,
+            image: true,
+          },
+          where: { is_featured: true },
+        });
+      },
+      3600 // 快取 1 小時
+    );
+
+    res.status(200).json({
+      message: "成功",
+      status: true,
+      data: FeaturedCategoriesInfo,
+    });
+  } catch (err) {
+    logger.error("取得精選分類失敗", err);
+    next(new AppError(500, "取得精選分類失敗"));
+  }
 }
 
 module.exports = {
