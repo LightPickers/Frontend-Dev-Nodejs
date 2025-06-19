a9a1353853eb9d45824e3b3f6fa8bc0722547890
Angela-Chu <angelachu1598753@gmail.com>
2025-06-19 12:47:11 +0800

    直接於藍新付款完成後寄送訂單通知信

--- controllers/neWebPay.js
+++ controllers/neWebPay.js
@@ -118,6 +118,54 @@ async function postNotify(req, res, next) {
       await productRepo.save(products);
     });
 
+    // 加入寄送訂單確認Email
+    const userRepo = manager.getRepository("Users");
+    const user = await userRepo.findOneBy({ id: order.user_id });
+
+    const orderItemsFull = await manager
+      .getRepository("Order_items")
+      .find({ where: { order_id: order.id }, relations: { Products: true } });
+
+    const productList = orderItemsFull.map((item) => ({
+      name: item.Products.name,
+      quantity: item.quantity,
+      price: item.Products.selling_price,
+    }));
+
+    // 從訂單中取得 coupon_id
+    const couponRepo = manager.getRepository("Coupons");
+
+    let discountRate = 0; // 預設沒有折扣
+    if (order.coupon_id) {
+      const coupon = await couponRepo.findOne({
+        where: { id: order.coupon_id },
+        select: ["discount"],
+      });
+
+      if (coupon && typeof coupon.discount === "number") {
+        discountRate = coupon.discount * 0.1; // 例如 8 -> 0.8
+      }
+    }
+
+    // 計算折扣金額
+    const subtotal = order.amount - 60;
+    const discountAmount = Math.round(subtotal * (1 - discountRate));
+
+    await orderConfirm(user.email, {
+      customerName: user.name,
+      orderNumber: order.merchant_order_no,
+      orderDate: order.created_at,
+      products: productList,
+      subtotal: subtotal,
+      shippingFee: 60,
+      discount: discountAmount,
+      total: order.amount,
+      paymentMethod: order.payment_method,
+      recipientName: user.name,
+      recipientPhone: user.phone,
+      recipientAddress: `${user.address_zipcode} ${user.address_detail}`,
+    });
+
     return res.status(200).send("OK");
   } catch (err) {
     logger.error("藍新通知處理失敗：", err);
