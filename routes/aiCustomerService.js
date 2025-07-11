dca88dcddb743e0b9815e2030cc2a95cfb77c758
Tau Hsu <jason850629@gmail.com>
2025-07-11 18:43:09 +0800

    Update aiCustomerService.js

--- routes/aiCustomerService.js
+++ routes/aiCustomerService.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const auth = require("../middlewares/auth");
 const aiLimiter = require("../middlewares/rateLimiter");
+const blockBannedWords = require("../middlewares/blockBannedWords");
 const aiCustomerServiceController = require("../controllers/aiCustomerService");
 const handleErrorAsync = require("../utils/handleErrorAsync");
 
@@ -9,6 +10,7 @@ router.post(
   "/",
   auth,
   aiLimiter, // 1 分鐘內請求不得超過 10 次
+  blockBannedWords("message"), // 過濾 req.body.message
   handleErrorAsync(aiCustomerServiceController.postAiCustomerService)
 );
 
